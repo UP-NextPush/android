37440947d1f83429ac6ab7907c2242578f6721d5
Conny Duck <git@connyduck.at>
2023-06-10 08:40:01 +0200

    Simplify StartActivity layout

--- app/src/main/java/org/unifiedpush/distributor/nextpush/activities/StartActivity.kt
+++ app/src/main/java/org/unifiedpush/distributor/nextpush/activities/StartActivity.kt
@@ -4,17 +4,14 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.InputType
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
-import android.widget.RelativeLayout
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
-import com.google.android.material.textfield.TextInputEditText
 import org.unifiedpush.distributor.nextpush.R
 import org.unifiedpush.distributor.nextpush.account.Account
 import org.unifiedpush.distributor.nextpush.account.Account.setTypeDirect
@@ -25,7 +22,6 @@ import org.unifiedpush.distributor.nextpush.utils.TAG
 
 class StartActivity : AppCompatActivity() {
     private var onResult: ((activity: Activity, requestCode: Int, resultCode: Int, data: Intent?, block: (success: Boolean) -> Unit) -> Unit)? = null
-    private var passwordIsVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,17 +49,9 @@ class StartActivity : AppCompatActivity() {
             login()
         }
         findViewById<TextView>(R.id.manual_login).setOnClickListener {
-            findViewById<RelativeLayout>(R.id.manual_login_wrapper).apply {
+            findViewById<LinearLayout>(R.id.manual_login_wrapper).apply {
                 isGone = !isGone
             }
-        }
-        findViewById<ImageView>(R.id.ic_show_password).setOnClickListener {
-            passwordIsVisible = !passwordIsVisible
-            findViewById<TextInputEditText>(R.id.edt_password).inputType =
-                when (passwordIsVisible) {
-                    false -> InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-                    true -> InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-                }
         }
     }
 
